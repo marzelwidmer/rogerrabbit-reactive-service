9c4767b64cff661a01a20e5aff541b53e682cf50
Marcel Widmer <marzelwidmer@gmail.com>
2017-10-18 17:46:38 +0200

    add bootstrap UI

--- src/main/java/ch/keepcalm/microservice/rr/reactive/config/WebConfiguration.java
+++ src/main/java/ch/keepcalm/microservice/rr/reactive/config/WebConfiguration.java
@@ -1,10 +1,14 @@
 package ch.keepcalm.microservice.rr.reactive.config;
 
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
 
 
 /**
@@ -29,6 +33,13 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Mapper dozerBeanMapper() {
+        DozerBeanMapper beanMapper = new DozerBeanMapper();
+        beanMapper.setMappingFiles(Collections.singletonList("dozerJdk8Converters.xml"));
+        return beanMapper;
     }
 
 }
